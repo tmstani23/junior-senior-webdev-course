e416a06dfc8000a4eee20ccce299fec55f5cae6f
tmstani23 <tmstani23@gmail.com>
2020-08-17 15:24:50 -0500

    robo3
    Created components that get the card data, pass data as props to a cardList and card then render a list of cards on screen.  Added components and functionality for a searchbox and filtering the list or robots to display only those with the same name as what was searched.  Added scroll component that allows the user to scroll down through the robots.

--- react_redux_project/robo/src/components/Card.js
+++ react_redux_project/robo/src/components/Card.js
@@ -4,8 +4,7 @@ import React, {Component} from 'react';
 class Card extends Component {
     render() {
         return (
-            <div className ='tc bw2 shadow5 bg-light-green dib br3 pa3 ma2 grow'>
-                <h1>Robo-Friends</h1>
+            <div className ='bg-animate hover-bg-light-blue tc bw2 shadow5 bg-light-green dib br3 pa3 ma2 grow'>
                 <img alt='robots' src={`https://robohash.org/${this.props.id}?200x200`}/>
                 <h2>{this.props.name}</h2>
                 <p>{this.props.email}</p>
--- react_redux_project/robo/src/index.js
+++ react_redux_project/robo/src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'tachyons';
-import Card from './Card';
-import {robots} from './robots'
+import App from './containers/App';
+import './styles/App.css';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-      <Card id={robots[3].id} name={robots[3].name} email={robots[3].email} />
+      <App />
     </div>
     
   </React.StrictMode>,
