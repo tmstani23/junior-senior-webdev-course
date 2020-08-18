1ef3aea3beca26b636a8749903f763fdfde39234
tmstani23 <tmstani23@gmail.com>
2020-08-18 06:34:26 -0500

    Updates

---
+++ precache-manifest.ba429bb1ab2257c2505e24dba87baf27.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee5ff5a9045d303b13d300e36b55682",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/index.html"
+  },
+  {
+    "revision": "9bc0fa067af06f90289a",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "cd97cef90ac9c35a003b",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/css/main.5ece815f.chunk.css"
+  },
+  {
+    "revision": "9bc0fa067af06f90289a",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/js/2.16258756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/js/2.16258756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd97cef90ac9c35a003b",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/js/main.2058dd44.chunk.js"
+  },
+  {
+    "revision": "5247482d41a14a0e0be0",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/js/runtime-main.599bdcec.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/junior-senior-webdev-course/react_redux_project/robo/static/media/SEGA.82e09f78.TTF"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/robo/precache-manifest.ae8908c97428bb5d39ef3de01ba16fde.js"
+  "/junior-senior-webdev-course/react_redux_project/robo/precache-manifest.ba429bb1ab2257c2505e24dba87baf27.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/robo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/junior-senior-webdev-course/react_redux_project/robo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
