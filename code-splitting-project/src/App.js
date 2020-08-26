ce21dc733da7c080a83daeda049048ee07a87054
tmstani23 <tmstani23@gmail.com>
2020-08-26 14:21:03 -0500

    Code Splitting 4
    Added reactLazy components added logic for dynamically rendering page components using reactLazy.  The application now only serves js chunks when each page button is clicked.

--- code-splitting-project/src/App.js
+++ code-splitting-project/src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css';
 import Page1 from './components/page1';
+//Part1: no code splitting
 //import Page2 from './components/page2';
 //import Page3 from './components/page3';
-import AsyncComponent from './components/AsyncComponent'
+// Part 3: Cleaner code splitting using async HoC component
+//import AsyncComponent from './components/AsyncComponent'
+// Part 4: React.lazy
+const Page2Lazy = React.lazy(() => import('./components/page2'));
+const Page3Lazy = React.lazy(() => import('./components/page3'));
 
 class App extends React.Component {
   
@@ -17,6 +22,7 @@ class App extends React.Component {
     this.setState({
       route: route
     })
+    //Part 2: Dynamic rendering with conditional imports
     //With code splitting
     // if (route === 'page1') {
     //   this.setState({route: route})
@@ -37,19 +43,21 @@ class App extends React.Component {
 
   render() {
     const route = this.state.route;
+    
+    //Part 3: Async HOC component 
     //Dynamically load each component based on current route  
-    if (route === 'page1') {
-      return <Page1 onRouteChange={this.onRouteChange} />
-    } 
-    else if (route === 'page2') {
-      //AsyncPage2 is a higher order component that returns the imported component.
-      const AsyncPage2 = AsyncComponent(() => import('./components/page2'))
-      return <AsyncPage2 onRouteChange={this.onRouteChange} />
-    } 
-    else if (route === 'page3') {
-      const AsyncPage3 = AsyncComponent(() => import('./components/page3'))
-      return <AsyncPage3 onRouteChange={this.onRouteChange} />
-    }
+    // if (route === 'page1') {
+    //   return <Page1 onRouteChange={this.onRouteChange} />
+    // } 
+    // else if (route === 'page2') {
+    //   //AsyncPage2 is a higher order component that returns the imported component.
+    //   const AsyncPage2 = AsyncComponent(() => import('./components/page2'))
+    //   return <AsyncPage2 onRouteChange={this.onRouteChange} />
+    // } 
+    // else if (route === 'page3') {
+    //   const AsyncPage3 = AsyncComponent(() => import('./components/page3'))
+    //   return <AsyncPage3 onRouteChange={this.onRouteChange} />
+    // }
     
     
     // if(route === 'page1') {
@@ -58,10 +66,34 @@ class App extends React.Component {
     // else {
     //   return <this.state.component onRouteChange={this.onRouteChange} />
     // }
-        
-    
-  }
-  
+
+    // Part 4: using React lazy to render
+   
+    if (route === 'page1') {
+      // Suspense is a react component used for handling preload rendering and providing loading message
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page1 onRouteChange={this.onRouteChange} />
+        </Suspense>
+      )
+    } 
+    else if (route === 'page2') {
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page2Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      )
+      
+    } 
+    else if (route === 'page3') {
+      return (
+        <Suspense fallback={<div>Loading...</div>}>
+          <Page3Lazy onRouteChange={this.onRouteChange} />
+        </Suspense>
+      )
+       
+    }
+  } 
 }
 
 export default App;
