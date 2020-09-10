6c0201fd8a73c6defa25de83fef24b4d03baf8a5
tmstani23 <tmstani23@gmail.com>
2020-09-10 11:03:17 -0500

    sec update
    Added skeleton server and basic front end for testing sql injection and xss attacks

--- security_project/client/README.md
+++ security_project/client/README.md
@@ -3,3 +3,7 @@ For Udemy course - Security
 
 Run in conjunction with security-server-exercise repo
 
+run npm audit --audit-level high
+-this will audit entire package for vulns
+-can run npm audit fix after this to fix the vulns
+
--- security_project/server/server.js
+++ security_project/server/server.js
@@ -2,18 +2,30 @@ const express = require('express')
 const cors = require('cors')
 const helmet = require('helmet')
 var winston = require('winston');
+var morgan = require('morgan');
 
 const bodyParser = require('body-parser');
 const app = express()
 app.use(cors())
-app.use(helmet())
+//helmet covers most necessary headers and prevents attacks
+app.use(helmet());
+app.use(morgan('combined'));
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => {
+  //prevent cross site scripting and Cros Site Resource Fabrication attacks
+  res.cookie('session', '1', {httpOnly: true, secure: true});
+  //res.cookie('session', '1', {secure: true});
+  res.set({
+    'Content-Security-Policy': "script-src 'self' 'https://www.google.com/'"
+  })
+
+  res.send('Hello World!')
+})
 
 app.post('/secret', (req, res) => {
   const { userInput } = req.body;
-  console.log(userInput);
+  //console.log(userInput);
   if (userInput) {
     winston.log('info', 'user input: ' + userInput);
     res.status(200).json('success');
