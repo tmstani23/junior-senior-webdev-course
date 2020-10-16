946a3898d6ec9e0e0a67bc51fd31766591ff3619
tmstani23 <tmstani23@gmail.com>
2020-10-16 12:53:01 -0500

    serverless 03
    Added function that returns an emoji based on the url params that are sent to the aws lambda function.

--- serverless-test-proj/handler.js
+++ serverless-test-proj/handler.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const emojis = [
+  '😀','😄','😁','😆','🧐','🤖','🛫','🚀','🛸'
+]
+
 module.exports.rank = async event => {
+  const rank = event.queryStringParameters.rank;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank]
   return {
     statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     body: JSON.stringify(
       {
         message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        input: rankEmoji,
       },
       null,
       2
